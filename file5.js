7111e005226d5889b9d84c39916a7acdca614507
TDFleury <117152247+TDFleury@users.noreply.github.com>
2022-11-08 01:20:21 +0300

    Add files via upload

---
+++ file5.js
@@ -0,0 +1,21 @@
+
+
+function myfun(){
+
+ let a = document.getElementById('kantite').value;
+ let b = document.getElementById('price').value;
+  document.getElementById('result').innerHTML=(a*b);;
+   
+ 
+}
+
+/*
+Develop an order price calculator on JavaScript. The calculator should contain:
+- text box for entering the price of goods;
+- text box for entering quantity of goods;
+- the price calculation button.
+When you click on a button on the traveller, the price of the order, calculated as
+Product of price and quantity.
+ */
+
+ 
